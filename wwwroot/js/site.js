5c09f23e8fe595972335072b4fba8f73683f00c8
amats6655 <amats6655@gmail.com>
2023-10-21 01:47:05 +0300

    переработана группировка и фильтрация заявок. настройка фильтрации перенесена в popup. добавлена возможность фильтрации по нескольким признакам добавлено сохранение в сессию параметров фильтрации

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function() {
+    // Открытие модального окна
+    $("#openFilterPopup").click(function() {
+        $("#filterPopup").fadeIn();
+    });
+
+    // Закрытие модального окна
+    $("#closeFilterPopup").click(function() {
+        $("#filterPopup").fadeOut();
+    });
+});
+
--- wwwroot/js/tags.js
+++ wwwroot/js/tags.js
@@ -1,75 +1,92 @@
-﻿$( function() {
-    var $grid = $('.grid').isotope({
+﻿document.addEventListener("DOMContentLoaded", function() {
+    var filters = {};
+    // Проверяем, есть ли данные в sessionStorage
+    if (sessionStorage.getItem('filtersData')) {
+        // Если есть, извлекаем и десериализуем их
+        filters = JSON.parse(sessionStorage.getItem('filtersData'));
+    }
+
+    var grid = document.querySelector('.grid');
+    var iso = new Isotope(grid, {
         itemSelector: 'article'
     });
 
-    // filter buttons
-    $('.filters-button-group').on( 'click', 'button', function() {
-        var filterValue = $( this ).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-    $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'button', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
+
+
+    function combineFilters() {
+        var output = [];
+        var keys = Object.keys(filters);
+
+        function recurse(currIndex, currFilter) {
+            if (currIndex === keys.length) {
+                output.push(currFilter);
+                return;
+            }
+
+            for (var i = 0; i < filters[keys[currIndex]].length; i++) {
+                recurse(currIndex + 1, currFilter + filters[keys[currIndex]][i]);
+            }
+        }
+
+        if (keys.length > 0) {
+            recurse(0, "");
+        }
+
+        return output.length > 0 ? output.join(",") : "*";
+    }
+
+    function applyFilters() {
+        var filterValue = combineFilters();
+        iso.arrange({ filter: filterValue });
+    }
+
+    var checkboxes = document.querySelectorAll('.dropdown-menu .filter-checkbox');
+    checkboxes.forEach(function(checkbox) {
+        var group = checkbox.dataset.group;
+        var filterValue = checkbox.dataset.filter;
+        if (filters[group] && filters[group].includes(filterValue)) {
+            checkbox.checked = true;
+            checkbox.parentElement.parentElement.classList.add('selected-filter');
+        }
+        checkbox.addEventListener('change', function() {
+            var group = this.dataset.group;
+            var filterValue = this.dataset.filter;
+
+            if (!this.checked) {
+                var index = filters[group].indexOf(filterValue);
+                if (index > -1) {
+                    filters[group].splice(index, 1);
+                    if (!filters[group].length) {
+                        delete filters[group];
+                    }
+                }
+                this.parentElement.parentElement.classList.remove('selected-filter');
+            } else {
+                if (!filters[group]) {
+                    filters[group] = [];
+                }
+                filters[group].push(filterValue);
+                this.parentElement.parentElement.classList.add('selected-filter');
+            }
+            applyFilters();
+            // Сохраняем данные фильтрации в sessionStorage после применения фильтров
+            var filtersString = JSON.stringify(filters);
+            sessionStorage.setItem('filtersData', filtersString);
         });
     });
-});
+    applyFilters()
 
-// debounce so filtering doesn't happen every millisecond
-function debounce( fn, threshold ) {
-    var timeout;
-    return function debounced() {
-        if ( timeout ) {
-            clearTimeout( timeout );
-        }
-        function delayed() {
-            fn();
-            timeout = null;
-        }
-        timeout = setTimeout( delayed, threshold || 100 );
-    }
-}
-
-$(window).bind("load", function() {
-    $('#all').click();
-});
-
-
-
-$(function (){
-    $('#status').click(function (){
-        $('.status').toggleClass('hidden');
-        $('.priority').addClass('hidden');
-        $('.building').addClass('hidden')
-        $('#priority').removeClass('hidden');
-        $('#building').removeClass('hidden');
-        $(this).toggleClass('hidden');
+    var allButton = document.getElementById('all');
+    allButton.addEventListener('click', function() {
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = false;
+            checkbox.parentElement.parentElement.classList.remove('selected-filter');
+        });
+        filters = {};
+        // Сохраняем данные фильтрации в sessionStorage после применения фильтров
+        var filtersString = JSON.stringify(filters);
+        sessionStorage.setItem('filtersData', filtersString);
+        applyFilters();
     });
-    $('#priority').click(function (){
-        $('.priority').toggleClass('hidden');
-        $('.status').addClass('hidden');
-        $('.building').addClass('hidden');
-        $('#status').removeClass('hidden');
-        $('#building').removeClass('hidden');
-        $(this).toggleClass('hidden')
-    });
-    $('#building').click(function (){
-        $('.building').toggleClass('hidden');
-        $('.priority').addClass('hidden');
-        $('.status').addClass('hidden');
-        $('#priority').removeClass('hidden');
-        $('#status').removeClass('hidden');
-        $(this).toggleClass('hidden');
-    })
-    $('#all').click(function (){
-        $('#priority').removeClass('hidden');
-        $('#status').removeClass('hidden');
-        $('#building').removeClass('hidden');
-        $('.priority').addClass('hidden');
-        $('.status').addClass('hidden');
-        $('.building').addClass('hidden');
-        
-    });
+    
 });
