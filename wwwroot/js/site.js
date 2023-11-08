70a0a62760345da21e025b5fd1d88d1518cb64aa
Андрей Мац <amats6655@gmail.com>
2023-11-08 16:11:23 +0300

    запускать модальное окно с помощью bootstrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,12 +1,1 @@
-﻿$(document).ready(function() {
-    // Открытие модального окна
-    $("#openFilterPopup").click(function() {
-        $("#filterPopup").fadeIn();
-    });
-
-    // Закрытие модального окна
-    $("#closeFilterPopup").click(function() {
-        $("#filterPopup").fadeOut();
-    });
-});
-
+﻿
