00c65bb8683cd5f105cc3b7ad382c7417028721c
amats6655 <amats6655@gmail.com>
2023-11-14 17:33:41 +0300

    вынести данные customFields в файл конфигурации

--- Controllers/IssuesController.cs
+++ Controllers/IssuesController.cs
@@ -9,76 +9,31 @@ namespace RedmineApp.Controllers;
 
 public class IssuesController : Controller
 {
+    private readonly IConfiguration _configuration;
     private readonly RedmineService _redmineService;
     private readonly INotyfService _notyf;
     private readonly IMemoryCache _cache;
+    private readonly Dictionary<string, string> _customFieldSettings;
+    private readonly Dictionary<string, Dictionary<int, string>> _customFields;
+    private readonly Dictionary<int, string> _buildings;
 
-    public IssuesController(RedmineService redmineService, INotyfService notyf, IMemoryCache cache)
+    public IssuesController(RedmineService redmineService, INotyfService notyf, IMemoryCache cache, IConfiguration configuration)
     {
+        _configuration = configuration;
         _redmineService = redmineService;
         _notyf = notyf;
         _cache = cache;
+        _buildings = _configuration.GetSection("CustomFields:Buildings").Get<Dictionary<int, string>>() 
+                     ?? new Dictionary<int, string>();
     }
     
     public async Task<IActionResult> Index()
     {
-        var buildings = new Dictionary<int, string>()
-        {
-            {347, "Автодром"},
-            {224, "Административные здания"},
-            {226, "Айсберг"},
-            {262, "Академия единоборств"},
-            {481, "Альфа 4*"},
-            {369, "Альфа Заповедный квартал"},
-            {368, "Альфа Морской квартал"},
-            {483, "Альфа Морской порт"},
-            {371, "Альфа Парковый квартал"},
-            {370, "Альфа Прибрежный квартал"},
-            {227, "Парусная"},
-            {256, "Бассейн на Воскресенской"},
-            {228, "Гамма Сириус"},
-            {351, "ГК Академии Единоборств"},
-            {223, "Дельта"},
-            {229, "Детский сад (Кампус)"},
-            {482, "Детский сад (Континентальный)"},
-            {231, "Детский сад (Фигурная)"},
-            {232, "Детский сад (Общинная)"},
-            {230, "Детсуий сад (Воскресенская)"},
-            {233, "Кампус"},
-            {234, "Комплекс трамплинов"},
-            {261, "Концертный комплекс"},
-            {235, "Корпус Спорт"},
-            {254, "Корпус Школа"},
-            {237, "Лицей на Воскресенской"},
-            {236, "Лицей на Международной"},
-            {353, "Медцентр Академии Единоборств"},
-            {314, "Московский офис"},
-            {239, "Олимпийский парк (площадь)"},
-            {240, "Омега Сириус"},
-            {241, "Отель Пульсар"},
-            {238, "Парк Науки и Искусства"},
-            {242, "Планетарий в Олимпийском парке"},
-            {315, "Санкт-Петербургский офиса"},
-            {255, "Санно-бобслейная трасса"},
-            {243, "Сигма А"},
-            {244, "Сигма Б"},
-            {245, "Сигма В, Г"},
-            {246, "Сириус-Арена"},
-            {247, "Склады РЖД"},
-            {248, "Спортивный парк (мыс Адлер)"},
-            {249, "Тренировочная арена (ТАХШ)"},
-            {250, "Тренировочный центр (ТЦФК)"},
-            {251, "Хостел S Hostel"},
-            {352, "ЦПС Академии Единоборств"},
-            {252, "Шайба"},
-            {253, "Школа №38"}
-        };
-        
         if (!_redmineService.IsSessionValid())
         {
             return RedirectToAction("Index", "Login");
         }
-            
+        
         // Попробовать получить список задач из кэша
         if (!_cache.TryGetValue("UserIssues", out List<Issue>? issues))
         {
@@ -86,7 +41,7 @@ public class IssuesController : Controller
             issues = await _redmineService.GetIssuesAsync(clientIp);
             var cacheEntryOptions = new MemoryCacheEntryOptions
             {
-                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1) // Кеширование на 2 минуты
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1) // Кеширование на 1 минуту
             };
             _cache.Set("UserIssues", issues, cacheEntryOptions);
         }
@@ -99,7 +54,7 @@ public class IssuesController : Controller
 
         ViewData["IssueBuildings"] = issueBuildings;
         ViewData["UniqueBuildings"] = uniqueBuildings;
-        ViewData["Buildings"] = buildings;
+        ViewData["Buildings"] = _buildings;
         return View(issues);
     }
 
@@ -156,11 +111,9 @@ public class IssuesController : Controller
                 _notyf.Error(ex.Message);
                 return RedirectToAction("Index");
             }
-            else
-            {
-                _notyf.Error("Произошла ошибка при попытке взять задачу в работу");
-                return RedirectToAction("Index");
-            }
+
+            _notyf.Error("Произошла ошибка при попытке взять задачу в работу");
+            return RedirectToAction("Index");
         }
     }
     
