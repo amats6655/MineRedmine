d4c5b2bf8500fb9a395652d2c5aaa76056aaecac
amats6655 <amats6655@gmail.com>
2023-11-14 17:34:47 +0300

    переместить методы авторизации обратно в LoginController.cs

--- Controllers/IssuesController.cs
+++ Controllers/IssuesController.cs
@@ -116,41 +116,4 @@ public class IssuesController : Controller
             return RedirectToAction("Index");
         }
     }
-    
-    
-    [HttpPost]
-    public IActionResult ValidateApiKey(string apiKey)
-    {
-        var isValid = RedmineService.IsValidApiKey(apiKey);
-        
-        if (isValid)
-        {
-            HttpContext.Session.SetString("apiKey", apiKey);
-            HttpContext.Session.Remove("username");
-            HttpContext.Session.Remove("password");
-        }
-
-        return Json(new { isValid });
-    }
-
-    [HttpPost]
-    public IActionResult IsValidUserCredentials(string username, string password)
-    {
-        var isValid = RedmineService.IsValidUserCredentials(username, password);
-        if (isValid)
-        {
-            HttpContext.Session.SetString("username", username);
-            HttpContext.Session.SetString("password", password);
-            HttpContext.Session.Remove("apiKey");
-        }
-
-        return Json(new { isValid });
-    }
-    
-    public IActionResult Logout()
-    {
-        HttpContext.Session.Clear();
-
-        return RedirectToAction("Index");
-    }
 }
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,19 +5,26 @@ namespace RedmineApp.Controllers;
 
 public class LoginController: Controller
 {
+    private readonly RedmineService _redmineService;
+    
+    public LoginController(RedmineService redmineService)
+    {
+        _redmineService = redmineService;
+    }
+    
     public IActionResult Index()
     {
         var apiKey = HttpContext.Session.GetString("apiKey");
         var username = HttpContext.Session.GetString("username");
         var password = HttpContext.Session.GetString("password");
 
-        if (!string.IsNullOrEmpty(apiKey) && RedmineService.IsValidApiKey(apiKey))
+        if (!string.IsNullOrEmpty(apiKey) && _redmineService.IsValidApiKey(apiKey))
         {
             return RedirectToAction("Index", "Issues");
         }
         else if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
         {
-            if (RedmineService.IsValidUserCredentials(username, password))
+            if (_redmineService.IsValidUserCredentials(username, password))
             {
                 return RedirectToAction("Index", "Issues");
             }
@@ -25,6 +32,41 @@ public class LoginController: Controller
 
         return View();
     }
+    
+    [HttpPost]
+    public IActionResult ValidateApiKey(string apiKey)
+    {
+        var isValid = _redmineService.IsValidApiKey(apiKey);
+        
+        if (isValid)
+        {
+            HttpContext.Session.SetString("apiKey", apiKey);
+            HttpContext.Session.Remove("username");
+            HttpContext.Session.Remove("password");
+        }
 
+        return Json(new { isValid });
+    }
+
+    [HttpPost]
+    public IActionResult IsValidUserCredentials(string username, string password)
+    {
+        var isValid = _redmineService.IsValidUserCredentials(username, password);
+        if (isValid)
+        {
+            HttpContext.Session.SetString("username", username);
+            HttpContext.Session.SetString("password", password);
+            HttpContext.Session.Remove("apiKey");
+        }
+
+        return Json(new { isValid });
+    }
+    
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+
+        return RedirectToAction("Index");
+    }
 
 }
