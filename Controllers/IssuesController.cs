8f8529c0cd3503ae7bf4d95f11773190703ca5d7
Андрей Мац <amats6655@gmail.com>
2023-11-15 21:54:25 +0300

    for checkpoint

--- Controllers/IssuesController.cs
+++ Controllers/IssuesController.cs
@@ -167,4 +167,39 @@ public class IssuesController : Controller
             return RedirectToAction("Index");
         }
     }
+    [HttpPost]
+    public IActionResult ValidateApiKey(string apiKey)
+    {
+        var isValid = _redmineService.IsValidApiKey(apiKey);
+        
+        if (isValid)
+        {
+            HttpContext.Session.SetString("apiKey", apiKey);
+            HttpContext.Session.Remove("username");
+            HttpContext.Session.Remove("password");
+        }
+
+        return Json(new { isValid });
+    }
+
+    [HttpPost]
+    public IActionResult IsValidUserCredentials(string username, string password)
+    {
+        var isValid = _redmineService.IsValidUserCredentials(username, password);
+        if (isValid)
+        {
+            HttpContext.Session.SetString("username", username);
+            HttpContext.Session.SetString("password", password);
+            HttpContext.Session.Remove("apiKey");
+        }
+
+        return Json(new { isValid });
+    }
+    
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+
+        return RedirectToAction("Index");
+    }
 }
