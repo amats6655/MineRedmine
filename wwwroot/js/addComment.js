aa0849f3ec353e29ae7f3984e9ccbbd6c09e068a
amats6655 <amats6655@gmail.com>
2023-11-15 22:24:36 +0300

    добавить валидацию. не пустой комментарий, размер файла < 5MB

--- wwwroot/js/addComment.js
+++ wwwroot/js/addComment.js
@@ -1,29 +1,60 @@
-$(document).ready(function(){
-    $('#commentForm').on('submit', function (event){
+$(document).ready(function() {
+    $('#commentForm').on('submit', function(event) {
         event.preventDefault();
-        $('#loadingSpinner').show();
-        
-        var formData = new FormData(this);
-        var issueId = $('.comment-btn').data('issue-id');
-        var url = $('.comment-btn').data('url');
-        
-        formData.append('id', issueId);
-        formData.append('comment', $('#commentText').val());
-        formData.append('privateNotes', $('#privateComment').is(':checked'));
-        formData.append('file', $('#commentFile')[0].files[0]);
-        
-        $.ajax({
-            url:url,
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function (response) {
-                location.reload();
-            },
-            error: function () {
-                console.error(this.error)
+
+        var isValid = true;
+        var commentText = $('#commentText');
+        var commentFile = $('#commentFile')[0];
+        var file = commentFile.files[0];
+
+        // Проверка комментария
+        if (!commentText.val().trim()) {
+            commentText.addClass('is-invalid');
+            isValid = false;
+        } else {
+            commentText.removeClass('is-invalid');
+        }
+
+        // Проверка размера файла (если файл выбран)
+        if (file && file.size > 5 * 1024 * 1024) { // 5 MB
+            $(commentFile).addClass('is-invalid');
+            isValid = false;
+        } else {
+            $(commentFile).removeClass('is-invalid');
+        }
+
+        // Если валидация успешна, отправляем форму
+        if (isValid) {
+            $('#loadingSpinner').show();
+
+            var formData = new FormData(this);
+            var issueId = $('.comment-btn').data('issue-id');
+            var url = $('.comment-btn').data('url');
+
+            formData.append('id', issueId);
+            formData.append('comment', commentText.val());
+            formData.append('privateNotes', $('#privateComment').is(':checked'));
+            if (file) {
+                formData.append('file', file);
             }
-        });
+
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    $('#loadingSpinner').hide();
+                    $('#commentPopup').modal('hide');
+                    location.reload(); // или другая логика по завершении
+                },
+                error: function(xhr, status, error) {
+                    $('#loadingSpinner').hide();
+                    console.error(error);
+                    // Отображение сообщения об ошибке
+                }
+            });
+        }
     });
 });
